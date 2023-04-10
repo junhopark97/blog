4f8e2ddf93f40176b2f759f8d236855517b9f8fc
k-kams <jh97.dev@gmail.com>
2023-04-11 00:40:25 +0900

    Update account-urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from accounts.views import UserRegisterView, UserLoginView
+from accounts.views import UserRegisterView, UserLoginView, UserVerifyView
 
 router = routers.DefaultRouter()
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
+    path('verify/', UserVerifyView.as_view(), name='verify'),
 ]
