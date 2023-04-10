3be43413fcfa843dfb479b74cdf50ccfc8f02712
k-kams <jh97.dev@gmail.com>
2023-04-10 19:55:08 +0900

    Update test-login, test-register

--- accounts/tests/test_account.py
+++ accounts/tests/test_account.py
@@ -1,4 +1,4 @@
-import json
+import jwt
 
 import pytest
 from django.contrib.auth import get_user_model
@@ -6,8 +6,9 @@ from django.urls import reverse
 from rest_framework import status
 
 from rest_framework.test import APIClient
-from rest_framework_simplejwt.tokens import RefreshToken
+from environ import Env
 
+env = Env()
 User = get_user_model()
 pytestmark = pytest.mark.django_db
 
@@ -21,71 +22,55 @@ def api_client():
 def user_data():
     return {
         'email': 'test@example.com',
+        'username': 'test',
         'password': 'password',
-        'phone_number': '01000000000',
+        'phone_number': '0100000000',
         'gender': User.GenderStatus.MALE,
     }
 
 
 @pytest.fixture
-def authenticated_api_client(api_client, user_data):
-    User.objects.create_user(**user_data)
-    response = api_client.post(reverse('token_obtain_pair'), {
-        'email': user_data['email'],
-        'password': user_data['password'],
-    }, format='json')
-    token = response.data['access']
-    api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-    return api_client
+def user_info():
+    return {
+        'email': 'test@example.com',
+        'password': 'qwer1234!',
+        'password2': 'qwer1234!',
+        'gender': User.GenderStatus.MALE,
+        'username': 'test',
+        'phone_number': '01000000000',
+    }
 
 
 def test_create_user(user_data):
     user = User.objects.create_user(**user_data)
     assert user.email == user_data['email']
     assert user.check_password(user_data['password'])
+    assert user.username == user_data['username']
     assert user.phone_number == user_data['phone_number']
     assert user.gender == user_data['gender']
     assert not user.is_staff
     assert not user.is_superuser
 
 
-def test_create_superuser(user_data):
-    user_data.update({
-        'is_staff': True,
-        'is_superuser': True,
-    })
-    with pytest.raises(ValueError):
-        User.objects.create_superuser(**user_data)
-
-    user_data.update({
-        'is_staff': True,
-        'is_superuser': True,
-        'is_active': True,
-    })
-    user = User.objects.create_superuser(**user_data)
-    assert user.email == user_data['email']
-    assert user.check_password(user_data['password'])
-    assert user.phone_number == user_data['phone_number']
-    assert user.gender == user_data['gender']
-    assert user.is_staff
-    assert user.is_superuser
-    assert user.is_active
-
-
-def test_register(api_client, user_data):
-    response = api_client.post(reverse('register'), user_data, format='json')
+def test_register(api_client, user_info):
+    response = api_client.post(reverse('register'), user_info, format='json')
     assert response.status_code == status.HTTP_201_CREATED
-    assert response.data['email'] == user_data['email']
+    assert response.data['email'] == user_info['email']
     assert 'password' not in response.data
-    assert response.data['phone_number'] == user_data['phone_number']
-    assert response.data['gender'] == user_data['gender']
+    assert response.data['phone_number'] == user_info['phone_number']
+    assert response.data['gender'] == user_info['gender']
 
-    token = response.data['token']
-    token_payload = json.loads(RefreshToken(token).payload)
+    access = response.data['token']['access']
+    verify_access = jwt.decode(access, env('SECRET_KEY'), algorithms=['HS256'])
+    refresh = response.data['token']['refresh']
+    verify_refresh = jwt.decode(refresh, env('SECRET_KEY'), algorithms=['HS256'])
 
-    assert token_payload['email'] == user_data['email']
-    assert token_payload['user_id'] == User.objects.get(email=user_data['email']).id
+    assert verify_access.get('email') == user_info['email']
+    assert verify_access.get('user_id') == str(User.objects.get(email=user_info['email']).id)
+    assert verify_refresh.get('email') == user_info['email']
+    assert verify_refresh.get('user_id') == str(User.objects.get(email=user_info['email']).id)
 
+    # Updated assertion to expect a 400 status code
     response = api_client.post(reverse('register'), {
         'email': 'test@example.com',
         'password': 'password',
@@ -96,33 +81,32 @@ def test_register(api_client, user_data):
 
 def test_login(api_client, user_data):
     User.objects.create_user(**user_data)
-    response = api_client.post(reverse('token_obtain_pair'), {
+    response = api_client.post(reverse('login'), {
         'email': user_data['email'],
         'password': user_data['password'],
     }, format='json')
     assert response.status_code == status.HTTP_200_OK
-    assert 'access' in response.data
-    assert 'refresh' in response.data
+    assert 'access_token' in response.data['token']
+    assert 'refresh_token' in response.data['token']
 
-    refresh = response.data['refresh']
-    access = response.data['access']
-    verify_refresh = RefreshToken(refresh)
-    verify_access = RefreshToken(access)
-    assert str(verify_refresh['email']) == user_data['email']
-    assert str(verify_access['email']) == user_data['email']
+    access = response.data['token']['access_token']
+    verify_access = jwt.decode(access, env('SECRET_KEY'), algorithms=['HS256'])
+    refresh = response.data['token']['refresh_token']
+    verify_refresh = jwt.decode(refresh, env('SECRET_KEY'), algorithms=['HS256'])
 
-    response = api_client.post(reverse('token_obtain_pair'), {
+    assert verify_access.get('email') == user_data['email']
+    assert verify_refresh.get('email') == user_data['email']
+
+    response = api_client.post(reverse('login'), {
         'email': user_data['email'],
         'password': 'wrongpassword',
     }, format='json')
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert 'access' not in response.data
-    assert 'refresh' not in response.data
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert 'token' not in response.data
 
-    response = api_client.post(reverse('token_obtain_pair'), {
+    response = api_client.post(reverse('login'), {
         'email': 'wrongemail@example.com',
         'password': user_data['password'],
     }, format='json')
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert 'access' not in response.data
-    assert 'refresh' not in response.data
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert 'token' not in response.data
