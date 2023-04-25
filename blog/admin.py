5c666abdd9f0ecfdbb83760ec66e17cd21fe8279
k-kams <jh97.dev@gmail.com>
2023-04-25 23:14:10 +0900

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from blog.models import Post, Category, Tag, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', 'tag_list', 'title', 'description', 'image', 'created_at', 'updated_at', 'like')
+    list_display = ('id', 'category', 'tag_list', 'title', 'image', 'like_count', 'created_at', 'updated_at',)
 
     def tag_list(self, obj):
         return ','.join([t.name for t in obj.tags.all()])
@@ -13,17 +13,22 @@ class PostAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         return super().get_queryset(request).prefetch_related('tags')
 
+    def like_count(self, obj):
+        return obj.likes.count()
+
+    like_count.short_description = 'like'
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
+    list_display = ('id', 'name',)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'post', 'author', 'short_content', 'created_at', 'updated_at')
+    list_display = ('id', 'post', 'author', 'short_content', 'created_at', 'updated_at',)
